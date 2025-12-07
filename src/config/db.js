e0a4921fa25a5ca6d96119f90c470ec09f55cccd
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2025-12-07 19:55:12 +0700

    fix prisma import for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "../generated/prisma/client/client.ts";
+import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
